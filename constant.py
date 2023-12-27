93cafc90978cca3932228dfe998a3a3f56ee5afd
Toluwani <tolukann5778@gmail.com>
2023-12-27 14:35:25 -0500

    Updating

---
+++ constant.py
@@ -0,0 +1,29 @@
+info = {
+   "Pronoun": "him",
+   "Name": "Toluwani",
+   "Full_Name": "Toluwani Olukanni",
+   "Intro": "An Electrical and Computer Engineering Student at Norwich University",
+   "About":"Hey there, I'm Vicky! I'm passionate about sharing my technical and data knowledge. My expertise covers a wide range of topics, from AI and Data Science to Blockchain, Cloud, and more. You can explore the wealth of free courses and resources I've created at cognitiveclass.ai.",
+   "Tableau":"https://public.tableau.com/app/profile/vickytc/viz/SunnybrookTeam/Overview",
+   "Medium":"https://medium.com/@vicky-note/about",
+   "City":"Philadelphia, Pennsylvania, United States of America",
+   "Photo":"""<a href=\"https://www.linkedin.com/in/toluwani-olukanni/\"><img src=\"https://sn-assets.s3.us.cloud-object-storage.appdomain.cloud/projects/profile.png" width=\"200\"   alt=\"Profile\" title=\"Profile\"></a>""",
+   "Email": "tolukann5778@gmail.com"
+}
+
+embed_rss= {
+    'rss':"""<div style="overflow-y: scroll; height:500px; background-color:white;"> <div id="retainable-rss-embed" 
+        data-rss="https://medium.com/feed/@vicky-note"
+        data-maxcols="3" 
+        data-layout="grid"
+        data-poststyle="inline" 
+        data-readmore="Read the rest" 
+        data-buttonclass="btn btn-primary" 
+        data-offset="0"></div></div> <script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script>"""
+}
+
+endorsements = {
+    "img1": "https://user-images.githubusercontent.com/90204593/238169843-12872392-f2f1-40a6-a353-c06a2fa602c5.png",
+    "img2": "https://user-images.githubusercontent.com/90204593/238171251-5f4c5597-84d4-4b4b-803c-afe74e739070.png",
+    "img3": "https://user-images.githubusercontent.com/90204593/238171242-53f7ceb3-1a71-4726-a7f5-67721419fef8.png"
+}
---
+++ pages/2_Resume.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import base64
+from constant import *
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+        
+local_css("style/style.css")
+
+st.sidebar.markdown(info['Photo'],unsafe_allow_html=True)
+
+st.title("📝 Resume")
+
+st.write("[Click here if it's blocked by your browser](https://drive.google.com/file/d/1yztCc5SYT4cZu02kO9WcWg8Kp9O06U0y/view?usp=sharing)")
+
+with open("images/Resume.pdf","rb") as f:
+      base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+      pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000mm" height="1000mm" type="application/pdf"></iframe>'
+      st.markdown(pdf_display, unsafe_allow_html=True)
+  
